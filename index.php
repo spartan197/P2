47cdde019a9e76fe641334057d4fbcf98b30941d
spartan197 <invincible.spartan@gmail.com>
2016-09-25 21:47:48 -0500

    Updating index.php

--- index.php
+++ index.php
@@ -11,24 +11,26 @@ ini_set('display_errors', 1);
 	<?php require 'logic.php'; ?>
 
 </head>
-<body>
+	<body>
 
-	<h1>Omar's Password Generator</h1><br>
+		<h1>Omar's Password Generator</h1><br>
+		<form method='GET' action='index.php'>
+			<label>Enter the number of words you would like your password to have</label><br>
+	    <label>(You cannot exceed 9)</label> &nbsp;&nbsp;
+	    <input type='number' name='words' id='words' autofocus min='1' max="9"><br><br>
 
-	<form method='GET' action='index.php'>
+			<label>Would you like to have a number or a symbol in your password?</label><br>
+	    <input type='checkbox' name='chk[]' id='chk' value='char'>
+			<label>Symbol</label><br>
+	    <input type='checkbox' name='chk[]' id='chk' value="num">
+	    <label>Number</label><br><br>
 
-		<label>Enter the number of words you would like your password to have</label><br>
-    <label>(You cannot exceed 9)</label> &nbsp;&nbsp;
-    <input type='number' name='words' id='words' autofocus min='1' max="9"><br><br>
-		<label>Would you like to have a number or a symbol in your password?</label><br>
+			<input type='hidden' name='chk[]' id='chk' value="num2" value="checked">
 
-    <input type='checkbox' name='chk[]' id='chk' value='char'>
-		<label>Symbol</label><br>
-    <input type='checkbox' name='chk[]' id='chk' value="num">
-    <label>Number</label><br><br>
-		<input type='submit' value='Generate Password' class='button'>
+			<input type='submit' value='Generate Password' class='button'>
 
-	</form><br>
-	<h2>Your new password is:</h2>
-<h3><?php echo $passwd?><h3><br>
-</body></html>
+		</form><br>
+		<h2>Your new password is:</h2>
+		<h3><?php echo $passwd?><h3><br>
+	</body>
+</html>
